a8c5262fdf5edbd665e513ccbd53fe20c1ee7bdc
byverdu <byverdu@gmail.com>
2019-07-21 20:55:34 +0100

    sort docker-compose config for api package

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,13 +1,13 @@
 import app from './app';
 
-const { PORT = 9000 } = process.env;
-const mongoHost = 'mars-rover_db';
+const { NODE_ENV = 'development', PORT = 9000 } = process.env;
+const mongoHost = NODE_ENV === 'development' ? 'localhost' : 'mars-rover_db';
 const MONGODB_URI = `mongodb://${mongoHost}:27017/mars-rover`;
 const { connectDatabase } = require('./database');
 
 (async () => {
   try {
-    await connectDatabase(MONGODB_URI, true);
+    await connectDatabase(MONGODB_URI, NODE_ENV !== 'production');
   } catch (error) {
     console.error('Could not connect to database', { error });
     throw error;
