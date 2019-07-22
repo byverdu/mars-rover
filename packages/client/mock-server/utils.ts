8e0c030accb34b287bd9bfb6ad8dbac0299fd1c2
Albert Vallverdu <albert.vallverdu@sky.uk>
2019-07-22 13:07:07 +0100

    add mock-server setup to UI

---
+++ packages/client/mock-server/utils.ts
@@ -0,0 +1,100 @@
+import { EnumCardinalPoints, EnumRoverStatus } from '../src/Models/enums';
+import { IPlateauPayload, IPlateau, IRover, IRoverPositionPayload } from '../src/Models/Interfaces';
+import { v4 } from 'uuid';
+import apiUtils from '../../api/src/utils';
+
+const { promisify } = require('util');
+const path = require('path');
+const fs = require('fs');
+
+interface Utils {
+  getContentForRoutes: () => Promise<any>;
+  postPlateau: (plateauPayload: IPlateauPayload, plateau: IPlateau[]) => any;
+}
+
+const readFileAsync = promisify(fs.readFile);
+const getPathToJsonMock = fileName => {
+  const mocksPath = 'e2e/fixtures';
+  const jsonPath = path.join(__dirname, '../', `${mocksPath}`, `${fileName}.json`);
+
+  return jsonPath;
+};
+
+export const utils: Utils = {
+  postPlateau: (plateauPayload: IPlateauPayload, plateau: IPlateau[]) => {
+    const plateauSize = plateauPayload.plateauSize.split('x');
+    const uuidPlateau = v4();
+    const rovers: IRover[] = plateauPayload.rovers.map((item: IRoverPositionPayload) => {
+      const position = item.position.split(' ');
+      const newPosition = apiUtils
+        .convertSourceInToCoords(item.position, item.steps)
+        .pop()
+        .split(' ');
+
+      return {
+        uuid: v4(),
+        uuidPlateau,
+        lastKnownPosition: {
+          rawFormat: `${position.join(' ')}`,
+          axis: {
+            x: Number(position[0]),
+            y: Number(position[1])
+          },
+          position: position[2] as EnumCardinalPoints
+        },
+        newPosition: {
+          rawFormat: `${newPosition.join(' ')}`,
+          axis: {
+            x: Number(newPosition[0]),
+            y: Number(newPosition[1])
+          },
+          position: newPosition[2] as EnumCardinalPoints
+        },
+        status: EnumRoverStatus.sleep,
+        stepsToNextPosition: {
+          steps: apiUtils.convertSourceInToCoords(item.position, item.steps),
+          source: item.steps
+        }
+      };
+    });
+
+    const newPlateau: IPlateau = {
+      uuid: uuidPlateau,
+      name: 'Mars',
+      size: {
+        width: Number(plateauSize[0]),
+        height: Number(plateauSize[1])
+      },
+      rovers
+    };
+
+    plateau = [newPlateau]
+
+    return plateau;
+  },
+  getContentForRoutes: () => {
+    let count = 0;
+    const paths = {};
+    const mapRoutesToJson = {
+      plateau: 'getPlateau'
+    };
+    const routes = Object.keys(mapRoutesToJson);
+
+    return new Promise((resolve, reject) => {
+      routes.forEach(async route => {
+        const fileName = mapRoutesToJson[route];
+
+        try {
+          const fileContent = await readFileAsync(getPathToJsonMock(fileName), { encoding: 'utf8' });
+          paths[route] = JSON.parse(fileContent);
+          count += 1;
+          if (count === routes.length) {
+            resolve(paths);
+          }
+        } catch (error) {
+          reject(error);
+        }
+      });
+    });
+  }
+};
