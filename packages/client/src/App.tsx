f756314d32a902fbc55042d4b7528bbad609627b
byverdu <byverdu@gmail.com>
2019-07-24 01:03:26 +0100

    solved jest loading non typescript assets

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
-// import logo from './logo.svg';
-// import './App.css';
+import logo from './logo.svg';
+import './App.scss';
 import axios from 'axios';
 import { EnumApiRoutes } from './Models/enums';
 
