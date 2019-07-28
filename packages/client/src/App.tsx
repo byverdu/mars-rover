be2a393c4a97586c98a5e9656d3df4bf34429b6b
byverdu <byverdu@gmail.com>
2019-07-28 14:54:09 +0100

    moved functionality that moves the rover from App to Plateau component

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -2,19 +2,19 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.scss';
 import axios from 'axios';
-import { EnumApiRoutes, EnumProxyAddress, EnumCardinalPoints } from './Models/enums';
+import {
+  EnumApiRoutes,
+  EnumProxyAddress,
+  EnumCardinalPoints
+} from './Models/enums';
 import DetailsWrapper from './Components/DetailsWrapper';
 import Plateau from './Components/Plateau';
 import { detailsWrapperText } from './config';
 import { IPlateauPayload, IRover } from './Models/Interfaces';
 
-
-// https://codepen.io/giana/pen/OrpdLK
-
 interface AppState {
   data: any;
-  actualRover: IRover;
-  updateComponent: boolean;
+  rovers: IRover[];
 }
 
 export default class App extends Component<{}, AppState> {
@@ -23,242 +23,36 @@ export default class App extends Component<{}, AppState> {
 
     this.state = {
       data: null,
-      actualRover: null,
-      updateComponent: true
+      rovers: null
     };
   }
 
   async componentDidMount() {
-    const url = `${EnumProxyAddress[process.env.NODE_ENV]}${
-      EnumApiRoutes.getPlateau
-    }`;
-    console.log(process.env.NODE_ENV);
     const result = await axios.get(EnumApiRoutes.getPlateau);
 
-    this.setState(
-      {
-        // data: result.data.plateau.pop()
-        data: result.data[0],
-        actualRover:  result.data[0].rovers[0]
-      },
-      () => {
-        let roversCount = this.state.data.rovers.length;
-    let index = 0;
-      const allRoverSteps: string[] = []
-      this.state.data.rovers.forEach((element: IRover) => {
-        allRoverSteps.push(...element.stepsToNextPosition.steps)
-      });
-      
-      let dataLenght = allRoverSteps.length;
-      // let dataLenght = this.state.actualRover.stepsToNextPosition.steps.length;
-      let counter = 0;
-
-      let interval;
-
-      setTimeout(() => {
-        interval = setInterval(() => {
-          if (counter === (dataLenght - 1)) {
-            clearInterval(interval)
-            if (index === roversCount) {
-              index += 1;
-            }
-            console.log('end', this.state.data);
-
-            // this.setState({
-            //   actualRover: {
-            //     ...this.state.data.rovers[1]
-            //   }
-            // })
-
-            this.setState({updateComponent: false})
-          }
-
-          
-          // const steps = counter === 0 ?
-          // this.state.actualRover.stepsToNextPosition.steps :
-          // this.state.actualRover.stepsToNextPosition.steps.slice(1)
-          const steps = counter === 0 ?
-          allRoverSteps :
-          allRoverSteps.slice(counter)
-          const step = steps[0]
-          
-          console.log('running', counter, dataLenght, step)
-          // this.setState({
-          //   actualRover: {
-          //     ...this.state.actualRover,
-          //     stepsToNextPosition: {
-          //       steps,
-          //       source: this.state.actualRover.stepsToNextPosition.source
-          //     },
-          //     lastKnownPosition: {
-          //       rawFormat: step,
-          //       position: EnumCardinalPoints[step.substring(4)],
-          //       axis: {
-          //         x: Number(step.substring(0,1)),
-          //         y: Number(step.substring(2,3))
-          //       }
-          //     }
-          //   }
-          // }, () => {
-          //   counter += 1;
-          // })
-          this.setState({
-            actualRover: {
-              ...this.state.actualRover,
-              stepsToNextPosition: {
-                steps,
-                source: this.state.actualRover.stepsToNextPosition.source
-              },
-              lastKnownPosition: {
-                rawFormat: step,
-                position: EnumCardinalPoints[step.substring(4)],
-                axis: {
-                  x: Number(step.substring(0,1)),
-                  y: Number(step.substring(2,3))
-                }
-              }
-            }
-          }, () => {
-            counter += 1;
-          })
-        }, 1500)
-      }, 1000)
-      }
-    );
+    if (Object.keys(result.data).length > 0) {
+      this.setState(
+        {
+          data: result.data,
+          rovers: result.data.rovers
+        },
+        () => this.forceUpdate()
+      );
+    }
   }
 
-  // shouldComponentUpdate() {
-  //   console.log(this.state.updateComponent, 'this.state.updateComponent should')
-  //   return this.state.updateComponent;
-  // }
-
-  componentWillReceiveProps() {
-    console.log(this.state.updateComponent, 'this.state.updateComponent will')
-    
-    // let roversCount = this.state.data.rovers.length;
-    // let index = 0;
-      
-    //   let dataLenght = this.state.data.rovers[index].stepsToNextPosition.steps.length;
-    //   let counter = 0;
-
-    //   let interval;
-
-    //   setTimeout(() => {
-    //     interval = setInterval(() => {
-    //       if (counter === dataLenght) {
-    //         clearInterval(interval)
-    //         if (index <= roversCount) {
-    //           index += 1;
-    //         }
-    //         console.log('end');
-
-    //         this.setState({updateComponent: false})
-    //       }
-
-    //       console.log('running', index)
-
-    //       const steps = this.state.actualRover.stepsToNextPosition.steps.slice(index)
-    //       const step = steps[index]
-
-    //       this.setState({
-    //         actualRover: {
-    //           ...this.state.actualRover,
-    //           stepsToNextPosition: {
-    //             steps: steps.slice(index),
-    //             source: this.state.actualRover.stepsToNextPosition.source
-    //           },
-    //           lastKnownPosition: {
-    //             rawFormat: step,
-    //             position: EnumCardinalPoints[step.substring(4)],
-    //             axis: {
-    //               x: Number(step.substring(0,1)),
-    //               y: Number(step.substring(2,3))
-    //             }
-    //           }
-    //         }
-    //       })
-    //       counter += 1;
-    //     }, 1500)
-    //   }, 1000)
-  }
-
-  runAsync = (rover: IRover, index) => {
-    // return new Promise((resolve) => {
-      const {stepsToNextPosition: {steps, source}} = rover;
-      // console.log('calling function for', steps.length)
-
-      // for (let index = 0; index <= (steps.length - 1); index++) {
-      //   console.log(index, 'inside loop', steps[index])
-        return setInterval(() => {
-          console.log('resolved for =>', steps[index])
-          this.setState({
-            actualRover: {
-              ...this.state.actualRover,
-              stepsToNextPosition: {
-                steps: steps.slice(index),
-                source
-              },
-              lastKnownPosition: {
-                rawFormat: steps[index],
-                position: EnumCardinalPoints[steps[index].substring(4)],
-                axis: {
-                  x: Number(steps[index].substring(0,1)),
-                  y: Number(steps[index].substring(2,3))
-                }
-              }
-            }
-          })
-        }, 1500)
-      // }
-    // });
-  }
-
-  // runAsync = (rover: IRover) => {
-  //   return new Promise((resolve) => {
-  //     const {stepsToNextPosition: {steps, source}} = rover;
-  //     console.log('calling function for', steps.length)
-
-  //     for (let index = 0; index <= (steps.length - 1); index++) {
-  //       console.log(index, 'inside loop', steps[index])
-  //       const id = setInterval(() => {
-  //         console.log('resolved for =>', steps[index])
-  //         this.setState({
-  //           actualRover: {
-  //             ...this.state.actualRover,
-  //             stepsToNextPosition: {
-  //               steps: steps.slice(index),
-  //               source
-  //             },
-  //             lastKnownPosition: {
-  //               rawFormat: steps[index],
-  //               position: EnumCardinalPoints[steps[index].substring(4)],
-  //               axis: {
-  //                 x: Number(steps[index].substring(0,1)),
-  //                 y: Number(steps[index].substring(2,3))
-  //               }
-  //             }
-  //           }
-  //         }, () => {
-  //           clearInterval(id);
-  //           resolve(true);
-  //         })
-  //       }, 1500)
-  //     }
-  //   });
-  // }
-
-  postPlateau = async (data: IPlateauPayload) => {
-    const result = await axios.post(EnumApiRoutes.postPlateau, data);
+  postPlateau = async (payload: IPlateauPayload) => {
+    const { data } = await axios.post(EnumApiRoutes.postPlateau, payload);
 
     this.setState({
-      data: result.data.data
+      data,
+      rovers: data.rovers
     });
   };
 
   render() {
-    const { data, actualRover } = this.state;
+    const { data, rovers } = this.state;
 
-    console.log(actualRover)
     return (
       <div className="App">
         <section>
@@ -270,7 +64,7 @@ export default class App extends Component<{}, AppState> {
             <Plateau
               width={data.size.width}
               height={data.size.height}
-              rover={actualRover}
+              rovers={rovers}
             />
           )}
         </section>
--- packages/client/src/Components/Plateau/index.tsx
+++ packages/client/src/Components/Plateau/index.tsx
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Plateau.scss';
 import PlateauSquare from '../PlateauSquare';
 import Rover from '../Rover';
 import { IRover } from '../../Models/Interfaces';
+import { EnumCardinalPoints } from '../../Models/enums';
+
+export interface PlateauProps {
+  width: number;
+  height: number;
+  rovers: IRover[];
+}
+
+export interface PlateauState {
+  roverMoving: IRover;
+  countRoversMoved: number;
+  rovers: IRover[];
+}
 
 function roverAxisFormatter(position: string) {
   const coords = position.split(' ');
@@ -12,7 +25,6 @@ function roverAxisFormatter(position: string) {
 
 function gridBuilder(width: number, height: number, rover: IRover) {
   const wrapper = [];
-  // const rover: IRover = rovers[0];
   const { rawFormat, position } = rover.lastKnownPosition;
   const nextStepsFormatted = rover.stepsToNextPosition.steps.map((step) =>
     roverAxisFormatter(step)
@@ -39,7 +51,6 @@ function gridBuilder(width: number, height: number, rover: IRover) {
               status={rover.status}
             />
           )}
-          {willBeBusy && <div>Will be busy</div>}
         </PlateauSquare>
       );
     }
@@ -51,21 +62,144 @@ function gridBuilder(width: number, height: number, rover: IRover) {
     counter += 1;
   }
 
-  // console.log(wrapper, 'Plateau.scss');
-
   return wrapper;
 }
 
-export interface PlateauProps {
-  width: number;
-  height: number;
-  rover: IRover;
+export default class Plateau extends Component<PlateauProps, PlateauState> {
+  constructor(props: PlateauProps) {
+    super(props);
+
+    this.state = {
+      rovers: props.rovers,
+      roverMoving: props.rovers[0],
+      countRoversMoved: 0
+    };
+  }
+
+  // TODO: refactor functionality to move rover so it's not duplicated
+  componentWillReceiveProps(props: PlateauProps, nextProps: PlateauProps) {
+    const { stepsToNextPosition } = props.rovers[0];
+    const stepsLength = stepsToNextPosition.steps.length;
+    const roversLength = props.rovers.length;
+    let stepsCounter = 0;
+    let roversCounter = 0;
+
+    let interval;
+
+    interval = setInterval(() => {
+      const steps =
+        stepsCounter === 0
+          ? stepsToNextPosition.steps
+          : stepsToNextPosition.steps.slice(stepsCounter);
+      const step = steps[0];
+
+      this.setState({
+        countRoversMoved: 0,
+        rovers: props.rovers,
+        roverMoving: {
+          ...props.rovers[0],
+          stepsToNextPosition: {
+            steps,
+            source: stepsToNextPosition.source
+          },
+          lastKnownPosition: {
+            rawFormat: step,
+            position: EnumCardinalPoints[step.substring(4)],
+            axis: {
+              x: Number(step.substring(0, 1)),
+              y: Number(step.substring(2, 3))
+            }
+          }
+        }
+      });
+      stepsCounter += 1;
+
+      if (stepsCounter === stepsLength) {
+        roversCounter += 1;
+        if (roversCounter < roversLength) {
+          // Minor delay to allow the last setInterval to finish
+          // before the next state is set
+          setTimeout(() => {
+            this.setState({
+              roverMoving: this.state.rovers[roversCounter],
+              countRoversMoved: roversCounter
+            });
+          }, 1000);
+        }
+
+        clearInterval(interval);
+      }
+    }, 1000);
+  }
+
+  componentDidUpdate(prevProps: PlateauProps, prevState: PlateauState) {
+    if (
+      this.state.countRoversMoved > 0 &&
+      this.state.roverMoving.uuid !== prevState.roverMoving.uuid
+    ) {
+      console.log(this.state.roverMoving.uuid);
+      const { countRoversMoved } = this.state;
+      const rover = this.state.rovers[countRoversMoved];
+      const { stepsToNextPosition } = this.state.rovers[countRoversMoved];
+      const stepsLength = stepsToNextPosition.steps.length;
+      const roversLength = this.state.rovers.length;
+      let stepsCounter = 0;
+      let roversCounter = countRoversMoved;
+
+      let interval;
+
+      interval = setInterval(() => {
+        const steps =
+          stepsCounter === 0
+            ? stepsToNextPosition.steps
+            : stepsToNextPosition.steps.slice(stepsCounter);
+        const step = steps[0];
+
+        this.setState({
+          roverMoving: {
+            ...rover,
+            stepsToNextPosition: {
+              steps,
+              source: stepsToNextPosition.source
+            },
+            lastKnownPosition: {
+              rawFormat: step,
+              position: EnumCardinalPoints[step.substring(4)],
+              axis: {
+                x: Number(step.substring(0, 1)),
+                y: Number(step.substring(2, 3))
+              }
+            }
+          }
+        });
+        stepsCounter += 1;
+
+        if (stepsCounter === stepsLength) {
+          roversCounter += 1;
+          if (roversCounter < roversLength) {
+            // Minor delay to allow the last setInterval to finish
+            // before the next state is set
+            setTimeout(() => {
+              this.setState({
+                roverMoving: this.state.rovers[roversCounter],
+                countRoversMoved: roversCounter
+              });
+            }, 1000);
+          }
+
+          clearInterval(interval);
+        }
+      }, 1000);
+    }
+  }
+
+  render() {
+    const { width, height } = this.props;
+    const { roverMoving } = this.state;
+    return (
+      <section className="plateau" style={{ width: `${width * 100}px` }}>
+        {gridBuilder(width, height, roverMoving)}
+      </section>
+    );
+  }
 }
-
-const Plateau: React.FC<PlateauProps> = ({ width, height, rover }) => (
-  <section className="plateau" style={{ width: `${width * 100}px` }}>
-    {gridBuilder(width, height, rover)}
-  </section>
-);
-
-export default Plateau;
